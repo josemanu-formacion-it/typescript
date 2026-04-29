7ed786e334625c797afd89d65994ddac7d807457
josemanu.formacion.it <josemanu.formacion.it@gmail.com>
2026-04-29 04:28:30 +0200

    Create types.ts

---
+++ modulo-1/src/types.ts
@@ -0,0 +1,11 @@
+/**
+ * Interface que define la estructura de estadísticas completas
+ */
+export interface EstadisticasResumen {
+  media: number;
+  mediana: number;
+  desviacionEstandar: number;
+  minimo: number;
+  maximo: number;
+  cantidad: number;
+}
